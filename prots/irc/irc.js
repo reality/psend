7a6df0a1e522df9233ca643c99cb73bdea765399
reality <tinmachin3@gmail.com>
2013-09-24 06:41:17 +0000

    basic functionality works for irc prot

--- prots/irc/irc.js
+++ prots/irc/irc.js
@@ -1,7 +1,20 @@
+var jsbot = require('jsbot');
+
 var irc = function(config) {
+    var instance = jsbot.createJSBot(config.nick);
+
     return {
-        'send': function() {
-            console.log('Literally an IRC placeholder.');
+        'send': function(contact, message) {
+            var user = contact[2];
+                server = contact[3];
+
+            // TODO: Make removeconnection in jsbot and such so can create
+            //   persistent connections to make this more efficient
+            instance.addConnection(server, server, 6667, null, function(event) {
+                instance.say(server, user, message);
+                instance.disconnect(server);
+            });
+            instance.connect(server);
         }
     }
 };
